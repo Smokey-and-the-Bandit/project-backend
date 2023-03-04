8936466083438d1adb1992e63f1eb1e7239643d0
mariuszbyczek <91824400+mariuszbyczek@users.noreply.github.com>
2023-03-04 14:31:21 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,14 +8,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+     """A dummy docstring."""
     return "<p>Hello, World!</p>"
 
 
 @app.route('/hello')
 def hello_with_name():
+     """A dummy docstring."""
     return render_template('home.html')
 
 
 @app.route('/namesake')
 def greetings():
+     """A dummy docstring."""
     return render_template('namesake.html', name=request.args['name'])
